418e54e4f25daaf9002c70c51e1568aeec02c4c6
huangcj <43933609+SubtleSpark@users.noreply.github.com>
2026-09-19 14:03:59 +0800

    refactor: isolate Web Awesome vendor bundle

---
+++ src/vendor.js
@@ -0,0 +1,3 @@
+import '@awesome.me/webawesome/dist/components/button/button.js';
+import '@awesome.me/webawesome/dist/components/color-picker/color-picker.js';
+import '@awesome.me/webawesome/dist/components/popover/popover.js';
